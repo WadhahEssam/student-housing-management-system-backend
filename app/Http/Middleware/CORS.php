c90d8a1e5caf5d4b1d6547093c60b34ac9714933
Wadah Esam <wadah-wleed@hotmail.com>
2018-10-04 04:39:33 +0300

    small fix to test

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -17,6 +17,7 @@ class CORS
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+            ->header('Content-Type', '*');
     }
 }
